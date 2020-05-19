8bd71e82c5049d74446c3f6ab6e441171809cd5f
Niket Sharma <niket.connect@gmail.com>
2020-05-19 16:07:46 +0530

    Ai Class can recognise the PlayerTank now

--- TankWars/Source/TankWars/Private/TankAiController.cpp
+++ TankWars/Source/TankWars/Private/TankAiController.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Engine/World.h"
 #include "TankAiController.h"
 
 
@@ -9,16 +9,16 @@ void ATankAiController::BeginPlay()
 
 	Super::BeginPlay();
 
-	auto ControlledTank = GetAiControlledTank();
+	auto ControlledTank = GetPlayerTank();
 
 	if (!ControlledTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Ai Controlled Tank failed"));
+		UE_LOG(LogTemp, Warning, TEXT("Ai could not get the Player Tank"));
 	}
 
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Debug Log: Ai Possessed Tank: %s"), *(ControlledTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("Debug Log: Ai got the Player Tank: %s"), *(ControlledTank->GetName()));
 	}
 }
 
@@ -26,4 +26,18 @@ ATank* ATankAiController::GetAiControlledTank() const
 {
 
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAiController::GetPlayerTank()
+{
+	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerTank)
+	{
+		return nullptr;
+	}
+
+	else
+	{
+		return Cast<ATank>(PlayerTank); //GetsFPController would return a PC, GetPawn returns a Pawn which we need
+	}
 }
--- TankWars/Source/TankWars/Public/TankAiController.h
+++ TankWars/Source/TankWars/Public/TankAiController.h
@@ -15,8 +15,10 @@ class TANKWARS_API ATankAiController : public AAIController
 {
 	GENERATED_BODY()
 
-public:
+private:
 	ATank* GetAiControlledTank() const;  //ATank because that is what this is returning 
 	
 	void BeginPlay() override; //Declaring this to use functions at Begin-Play event in the implementaion
+
+	ATank* GetPlayerTank();
 };
